acadf4caf066db95729c75164be4a52ee2f6c60a
Alexander <berezkin05@gmail.com>
2017-07-13 14:23:54 +0300

    last changes

---
+++ src/main/java/web/securityspring/controller/UserController.java
@@ -0,0 +1,69 @@
+package web.securityspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import web.securityspring.model.User;
+import web.securityspring.service.SecurityService;
+import web.securityspring.service.UserService;
+import web.securityspring.validator.UserValidator;
+
+/**
+ * Controller for {@link web.securityspring.model.User}'s pages
+ *
+ * @author Alexander
+ * @version 1.0
+ */
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getUsername(), userForm.getConfirmPassword());
+
+        return "redirect:/welcome";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("error", "Username or Password is incorrect");
+        }
+
+        if (logout!= null){
+            model.addAttribute("message", "Logged out successfully");
+        }
+
+        return "login";
+    }
+
+
+}
